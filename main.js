b3abb2f8d8f0aa09df39b186d353c869802861f1
ivan-kapelyukh <ivan.kapelyukh@gmail.com>
2018-12-26 21:16:07 +0000

    Determine user function name by parsing code

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 function acceptCode() {
   var input = document.getElementById("codeInput").value;
 
-  var startTime = performance.now();
-  var output = eval(input);
-  var endTime = performance.now();
+  // grabs prefix until first ( character, e.g. "function   myFunc   "
+  // then grabs second word, i.e. the function name
+  var funcName = input.substr(0, input.indexOf("(")).split(/\s+/)[1];
 
-  document.getElementById("output").innerHTML = "Runtime: " + Math.round(endTime - startTime) + " ms";
+  // var startTime = performance.now();
+  // var output = eval(input);
+  // var endTime = performance.now();
+
+  // document.getElementById("output").innerHTML = "Runtime: " + Math.round(endTime - startTime) + " ms";
+  document.getElementById("output").innerHTML = funcName;
 }
