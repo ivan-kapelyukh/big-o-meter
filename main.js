8e10c4cc4415acfc19857e12916ca26540377b3d
ivan-kapelyukh <ivan.kapelyukh@gmail.com>
2018-12-26 21:39:59 +0000

    Parse arguments to user function

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ function acceptCode() {
 
   // grabs prefix until first ( character, e.g. "function   myFunc   "
   // then grabs second word, i.e. the function name
-  var funcName = input.substr(0, input.indexOf("(")).split(/\s+/)[1];
+  var funcName = input.substring(0, input.indexOf("(")).split(/\s+/)[1];
+  var args = input.substring(input.indexOf("(") + 1, input.indexOf(")")).split(/[, ]+/);
 
   // var startTime = performance.now();
   // var output = eval(input);
   // var endTime = performance.now();
 
   // document.getElementById("output").innerHTML = "Runtime: " + Math.round(endTime - startTime) + " ms";
-  document.getElementById("output").innerHTML = funcName;
+  document.getElementById("output").innerHTML = args;
 }
