6be38e7e83258d3a49a8d2b32d8e409bce3a8c30
ivan-kapelyukh <ivan.kapelyukh@gmail.com>
2018-12-29 16:30:11 +0000

    Make graph able to draw multiple trendlines

--- graph.js
+++ graph.js
@@ -4,30 +4,22 @@ google.charts.setOnLoadCallback(function () {
   // document.getElementById("codeInput").scrollIntoView();
 });
 
-// takes array of x, y pair arrays, with first pair as headings
+// takes array of 4-tuple arrays, with first tuple as headings
 function drawGraph(dataArrays) {
   var data = google.visualization.arrayToDataTable(dataArrays);
+  var n = dataArrays.length;
 
   var hLabel = dataArrays[0][0];
-  var hMin = dataArrays[1][0];
-  var hMax = dataArrays[dataArrays.length - 1][0];
+  var hMax = dataArrays[n - 1][0];
 
   var vLabel = dataArrays[0][1];
-  var vMin = dataArrays[1][1];
-  var vMax = dataArrays[dataArrays.length - 1][1];
+  var vMax = Math.max(dataArrays[n - 1][1], dataArrays[n - 1][2], dataArrays[n - 1][3]);
 
   var options = {
     title: "Runtime vs input size",
     hAxis: {title: hLabel, minValue: 0, maxValue: Math.ceil(hMax)},
     vAxis: {title: vLabel, minValue: 0, maxValue: Math.ceil(vMax)},
-    legend: "none",
-    trendlines: {
-      0: {
-        type: 'polynomial',
-        degree: 2,
-        visibleInLegend: false,
-      }
-    }
+    legend: "none"
   };
 
   var graphElem = document.getElementById("chart-div");
