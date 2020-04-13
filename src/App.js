0750bb66309ab3fd1aad816d2c6587f56c96faa1
Ivan Kapelyukh <ivan.kapelyukh@gmail.com>
2020-04-13 15:35:20 +0100

    Extract function name from code input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { analyseCode } from "./functionAnalysis.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Hello there!</p>
+        <textarea id="editor" rows="20" cols="80"></textarea>
+
+        <br />
+
+        <button onclick="codeSubmitted()">Analyse</button>
+      </div>
+    );
+  }
+
+  static codeSubmitted() {
+    const code = document.getElementById("editor").getValue();
+    const funcName = App.extractFuncName(code);
+  }
+
+  static extractFuncName(code) {
+    // Get last word before opening bracket.
+    return code.substring(0, code.indexOf("(")).trim().split(" ").pop();
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("extracts normal function name", () => {
+  const code = "function makesFour() { return 2 + 2; }";
+  const funcName = App.extractFuncName(code);
+  expect(funcName).toBe("makesFour");
+});
+
+test("extracts name with weird spacing", () => {
+  const code = " function  makesSix  () { return 6 + 1; }";
+  const funcName = App.extractFuncName(code);
+  expect(funcName).toBe("makesSix");
 });
